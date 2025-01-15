5c5d3fdf7635b9308965c4af7d42bf3243c3f172
MUGDHA HAZRA <63424869+Mugdha-Hazra@users.noreply.github.com>
2025-01-15 23:08:55 +0530

    Update 119. Pascal's Triangle II.cpp

--- Google Questions/119. Pascal's Triangle II.cpp
+++ Google Questions/119. Pascal's Triangle II.cpp
@@ -1,1 +1,17 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> vi(rowIndex + 1);   // Create a vector of size rowIndex + 1
+        vi[0] = 1;                       // The first element in any row is always 1
+        
+        // This loop fills the row
+        for (int i = 0; i <= rowIndex; ++i) {
+            for (int j = i; j > 0; --j) {
+                vi[j] += vi[j - 1];   // Add the value of the previous element in the row
+            }
+        }
+        
+        return vi;
+    }
+};
 
